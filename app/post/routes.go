3db016b0f4966428151759c9a881d9c64f0cfd96
stnc <samtunc@yahooo.com>
2025-02-22 17:19:28 -0700

    added

---
+++ app/post/routes.go
@@ -0,0 +1,8 @@
+package cmsServices_mod
+
+import "github.com/gin-gonic/gin"
+
+func (m *Module) RegisterRoutes(router *gin.Engine) {
+	router.POST("/post", m.handler.CreatePost)
+	router.POST("/post/pubsub", m.handler.PublishPost)
+}
